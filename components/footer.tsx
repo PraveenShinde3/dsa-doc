6ad7e4b7fe27da296554efdd8e11a466d69b4509
Praveen Shinde <shindepraveen002@gmail.com>
2025-09-06 11:26:05 +0530

    refactor(footer): improve layout and links

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,38 @@
 import Link from "next/link";
-import { GripIcon } from "lucide-react";
+import { LogoDiv } from "./navbar";
 
 export function Footer() {
   return (
-    <footer className="border-t w-full h-16">
-      <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
+    <footer className="border-t w-full h-16 bg-background">
+      <div className="container flex items-center sm:justify-between justify-start gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
+        {/* Left Section */}
+        <div className="flex item-center gap-3">
+          <LogoDiv />
+          <p>
+            <span className="text-xs rounded-full bg-muted px-1 border border-border text-muted-foreground">
+              v1.0.0
+            </span>
+          </p>
+        </div>
         <div className="flex items-center gap-3">
-          <GripIcon className="sm:block hidden w-5 h-5 text-muted-foreground" />
           <p className="text-center">
-            Build by{" "}
+            © {new Date().getFullYear()} · Built by{" "}
             <Link
-              className="px-1 underline underline-offset-2"
               href="https://github.com/PraveenShinde3"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-1 underline underline-offset-2 hover:text-foreground"
             >
               Praveen Shinde
+            </Link>
+            · Powered by{" "}
+            <Link
+              href="https://ariadocs.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-1 underline underline-offset-2 hover:text-foreground"
+            >
+              Ariadocs
             </Link>
           </p>
         </div>
