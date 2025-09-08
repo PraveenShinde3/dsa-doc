d395e6f460023d0bee652188a7820dbf19366f02
Praveen Shinde <shindepraveen002@gmail.com>
2025-09-08 18:26:30 +0530

    feat: add and reference new images in public/images and update MDX content accordingly

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -6,7 +6,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative max-w-full overflow-hidden rounded-md border">
+  <div className="relative max-w-full overflow-x-auto rounded-md border">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm  ", className)}
