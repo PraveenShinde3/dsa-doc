f7ee83fa0b117416acc610faefb91e8a124097f3
Praveen Shinde <shindepraveen002@gmail.com>
2025-09-13 10:05:01 +0530

    UI(button & dropdow): Small Ui changes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors outline-none disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -20,11 +20,12 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-9 w-9",
-        xs: "h-7 rounded-md px-2",
+        default: "h-10 px-4 py-2 text-sm ",
+        sm: "h-9 rounded-md px-3 text-sm ",
+        lg: "h-11 rounded-md px-8 text-sm ",
+        icon: "h-9 w-9 text-sm ",
+        iconXs: "h-7 w-7 text-xs ",
+        xs: "h-7 rounded-md px-2 text-xs",
       },
     },
     defaultVariants: {
