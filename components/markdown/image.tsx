c03c6ba6be8dc88c4b4f27f33ff8eb805ca0e74c
Praveen Shinde <shindepraveen002@gmail.com>
2026-01-11 10:08:47 +0530

    fix: fixed makrdown/image.tsx file for scr field

--- components/markdown/image.tsx
+++ components/markdown/image.tsx
@@ -12,9 +12,13 @@ export default function Image({
   ...props
 }: ComponentProps<"img">) {
   if (!src) return null;
+  // Convert Blob to URL if needed
+  const resolvedSrc =
+    typeof src === "string" ? src : (URL.createObjectURL(src) as string);
+
   return (
     <NextImage
-      src={src}
+      src={resolvedSrc}
       alt={alt}
       width={width as Width}
       height={height as Height}
